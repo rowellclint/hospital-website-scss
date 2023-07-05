f5c3df6eba078503e5b1adf63dde5c09e3a9d51d
unknown <valera.rowellclint@ue.edu.ph>
2023-07-05 17:29:09 +0800

    Updated Version

---
+++ script.js
@@ -0,0 +1,3 @@
+const navItems = document.querySelectorAll("#nav__items");
+const openNavBtn = document.querySelectorAll("#open__nav-btn");
+const closeNavBtn = document.querySelectorAll("#close__nav-btn");

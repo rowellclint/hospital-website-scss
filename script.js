ef2ae0bf7c4957fd6ee941f24c97e6c37e9b8db9
rowellclint <rowellclint35@gmail.com>
2023-07-13 16:41:14 +0800

    Updated Version

--- script.js
+++
@@ -1,3 +0,0 @@
-const navItems = document.querySelectorAll("#nav__items");
-const openNavBtn = document.querySelectorAll("#open__nav-btn");
-const closeNavBtn = document.querySelectorAll("#close__nav-btn");
